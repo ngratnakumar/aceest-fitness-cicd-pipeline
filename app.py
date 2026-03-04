74bb3ff60a6898cde99569e2026b52a0e9404fc4
BOLLAPRAGADA NAGA RATNA KUMAR <2025ht66015@wilp.bits-pilani.ac.in>
2026-03-05 00:21:17 +0530

    Initial release: Basic UI and program data (v1.0.0)

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+DB_NAME = "aceest_fitness.db"
+
+# Creating initial SQLite 
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    conn.execute('''CREATE TABLE IF NOT EXISTS clients 
+                    (id INTEGER PRIMARY KEY AUTOINCREMEN, name TEXT UNIQUE, age INTEGER, weight REAL)''')
+    conn.commit()
+    conn.close()
+
+@app.route('/')
+def index():
+    conn = sqlite3.connect(DB_NAME)
+    clients = conn.execute('SELECT * FROM clients').fetchall()
+    conn.close()
+    return f"<h1>ACEest Fitness Dashboard</h1><p>Total Clients: {len(clients)}</p>"
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host='0.0.0.0', port=5000)
