de462ffd68b19836db5f55847d546445c56cf7d2
BOLLAPRAGADA NAGA RATNA KUMAR <2025ht66015@wilp.bits-pilani.ac.in>
2026-03-08 12:16:13 +0530

    feat: modified Gym App - included  temp data (v2.0.0)

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-import os
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-DB_NAME = "aceest_fitness.db"
 
-# Creating initial SQLite 
-def init_db():
-    conn = sqlite3.connect(DB_NAME)
-    conn.execute('''CREATE TABLE IF NOT EXISTS clients 
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, age INTEGER, weight REAL)''')
-    conn.commit()
-    conn.close()
-
-@app.before_request
-def setup():
-    init_db()
+members = [
+    {"id": 1, "name": "Rahul", "plan": "Premium"},
+    {"id": 2, "name": "Anita", "plan": "Basic"}
+]
 
 @app.route('/')
-def index():
-    conn = sqlite3.connect(DB_NAME)
-    clients = conn.execute('SELECT * FROM clients').fetchall()
-    conn.close()
-    return f"<h1>ACEest Fitness Dashboard</h1><p>Total Clients: {len(clients)}</p>"
+def home():
+    return "Welcome to ACEest Fitness & Gym"
 
-if __name__ == '__main__':
-    init_db()
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/members')
+def get_members():
+    return jsonify(members)
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    data = request.get_json()
+    members.append(data)
+    return {"message": "Member added"}, 201
+
+@app.route('/health')
+def health():
+    return {"status": "running"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
